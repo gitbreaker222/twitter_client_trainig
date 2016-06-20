339bac77dd17ef2479ac639e19c6e2283058992e
Ruben La Biunda <r.biunda@mwaysolutions.com>
2016-06-20 17:01:19 +0200

    assigns new controller to list template

--- app/main/main.js
+++ app/main/main.js
@@ -5,24 +5,24 @@ angular.module('main', [
   'ui.router',
   // TODO: load other modules selected during generation
 ])
-.config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  // ROUTING with ui.router
-  $urlRouterProvider.otherwise('/main/list');
-  $stateProvider
+    // ROUTING with ui.router
+    $urlRouterProvider.otherwise('/main/list');
+    $stateProvider
     // this state is placed in the <ion-nav-view> in the index.html
-    .state('main', {
-      url: '/main',
-      abstract: true,
-      templateUrl: 'main/templates/menu.html',
-      controller: 'MenuCtrl as menu'
-    })
+      .state('main', {
+        url: '/main',
+        abstract: true,
+        templateUrl: 'main/templates/menu.html',
+        controller: 'MenuCtrl as menu'
+      })
       .state('main.list', {
         url: '/list',
         views: {
           'pageContent': {
             templateUrl: 'main/templates/list.html',
-            // controller: '<someCtrl> as ctrl'
+            controller: 'ListCtrl as list'
           }
         }
       })
@@ -44,4 +44,4 @@ angular.module('main', [
           }
         }
       });
-});
+  });
