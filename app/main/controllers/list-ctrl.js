4f017e98500d1ebeea8bc4a496f514c3473cdea7
Ruben La Biunda <r.biunda@mwaysolutions.com>
2016-06-20 16:03:35 +0200

    inits controller for list template

---
+++ app/main/controllers/list-ctrl.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module('main')
+.controller('ListCtrl', function ($log) {
+
+  $log.log('Hello from your Controller: ListCtrl in module main:. This is your controller:', this);
+
+});
