e820f821a731c19355cad10b5efdccb532c5a754
Ruben La Biunda <r.biunda@mwaysolutions.com>
2016-06-20 16:57:38 +0200

    adds test data

--- app/main/controllers/list-ctrl.js
+++ app/main/controllers/list-ctrl.js
@@ -1,7 +1,23 @@
 'use strict';
 angular.module('main')
-.controller('ListCtrl', function ($log) {
+.controller('ListCtrl', function ($log, $scope) {
 
   $log.log('Hello from your Controller: ListCtrl in module main:. This is your controller:', this);
-
+  $scope.tweets = [
+    {
+      content: 'you need to checkout #KREATOR german #metal band',
+      user: 'tweeterSeppl',
+      timestamp: new Date().getTime()
+    },
+    {
+      content: 'girl I LOVE HEAVY METAL \\m/,',
+      user: 'tweeterLilli',
+      timestamp: new Date().getTime() + 50000000
+    },
+    {
+      content: 'going to see Powerman5000 next saturday :)',
+      user: 'tweeterPetruska',
+      timestamp: new Date().getTime() + 800000000
+    }
+  ];
 });
