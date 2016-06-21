951ca02146cca3205deee53b9896ca70b47627e0
Ruben La Biunda <r.biunda@mwaysolutions.com>
2016-06-21 17:22:15 +0200

    adds loading spinner

--- app/main/controllers/list-ctrl.js
+++ app/main/controllers/list-ctrl.js
@@ -1,19 +1,19 @@
 'use strict';
 angular.module('main')
-.controller('ListCtrl', function ($log, $scope, GetTweets) {
+  .controller('ListCtrl', function ($log, $scope, GetTweets) {
 
-  var that = this;
+    var that = this;
 
-  this.data = GetTweets.data;
-  this.loading = false;
+    this.data = GetTweets.data;
+    this.loading = false;
 
-  this.search = function(searchString){
-    that.loading = true;
-    GetTweets.get(searchString).then(function(){
-      that.loading = false;
-    }, function(){
-      that.loading = false;
-    });
-  }
+    this.search = function (searchString) {
+      that.loading = true;
+      GetTweets.get(searchString).then(function () {
+        that.loading = false;
+      }, function () {
+        that.loading = false;
+      });
+    };
 
-});
+  });
