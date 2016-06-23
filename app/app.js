283daae773975987889f40a5d9260918b504a2f0
Ruben La Biunda <r.biunda@mwaysolutions.com>
2016-06-23 17:18:34 +0200

    inits platform-information access

--- app/app.js
+++ app/app.js
@@ -1,5 +1,24 @@
 'use strict';
 angular.module('testrun_twitter', [
-  // load your modules here
-  'main', // starting with the main module
-]);
+  'main',
+  'ionic'
+])
+  .run(function ($log,
+                 $ionicPlatform) {
+
+    var deviceInformation = ionic.Platform.device();
+
+    var isWebView = ionic.Platform.isWebView();
+    var isIPad = ionic.Platform.isIPad();
+    var isIOS = ionic.Platform.isIOS();
+    var isAndroid = ionic.Platform.isAndroid();
+    var isWindowsPhone = ionic.Platform.isWindowsPhone();
+
+    var currentPlatform = ionic.Platform.platform();
+    var currentPlatformVersion = ionic.Platform.version();
+
+    $log.log(deviceInformation, isWebView, isIPad, isIOS, isAndroid, isWindowsPhone, currentPlatform, currentPlatformVersion);
+    $ionicPlatform.ready( function () {
+    });
+
+  });
