4a68efba839c0379893e623c747397852da10808
Ruben La Biunda <r.biunda@mwaysolutions.com>
2016-06-24 16:29:40 +0200

    adds open method to service

--- app/main/services/in-app-browser-serv.js
+++ app/main/services/in-app-browser-serv.js
@@ -1,7 +1,23 @@
 'use strict';
 angular.module('main')
-.service('InAppBrowser', function ($log) {
+  .service('InAppBrowser', function ($window) {
 
-  $log.log('Hello from your Service: InAppBrowser in module main');
+    this.open = function () {
+      $window.webview.openWebView(null, null, {
+        iconColor: ' #ffff00',
+        backgroundColor: '#f00000',
+        isPDF: false,
+        url: 'http://mwaysolutions.com',
+        urlEncoding: false,
+        visibleAddress: false,
+        editableAddress: false,
+        navigationAtTop: false,
+        icons: {
+          backward: true,
+          forward: true,
+          refresh: true
+        }
+      });
+    };
 
-});
+  });
